799425b36de63ab71543916efebfb8983cd12c74
heyenlow <57604869+heyenlow@users.noreply.github.com>
2020-11-11 23:11:03 -0600

    main

--- test.py
+++ test.py
@@ -1,3 +1,23 @@
 import numpy as np
+import tensorflow as tf
+import nltk as nltk
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
 print(np.version)
-print(np.add(3,4))
+print(tf.version)
+
+textFile = open("inputTest.txt", "r")
+textTest = textFile.read()
+
+
+#creates a dictionary of words and the number of their occurances
+def count_words(text):
+    words = {"test" : 0}
+    for word in text.split():
+        words[word] = words[word] + 1 
+    return words
+
+print(count_words(textTest))
+
